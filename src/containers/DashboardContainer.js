d22b663fa2aa0f1b4a8b86558c059ad50cbfe333
Ash Zhang <ash.norseman@gmail.com>
2016-06-15 09:00:36 +0800

    chart support

--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -6,24 +6,123 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+import echarts from 'echarts/lib/echarts';
+import 'echarts/lib/chart/bar';
+import 'echarts/lib/chart/line';
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/legend';
+import 'echarts/lib/component/markPoint';
+
 
 @connect(state => ({
-	user: state.user
+	user: state.user,
+  iteration: state.iteration
 }))
 export default class IterationContainer extends Component {
 
 	static propTypes = {
+    iteration: PropTypes.object,
 		user: PropTypes.object
 	};
 
+  componentDidMount() {
+    const chart = echarts.init(this.refs.taskChart),
+      list = this.props.iteration.iterationList,
+      xAxis = [],
+      finishedSeries = [],
+      pendingSeries = [],
+      total = [];
+
+    list.forEach(item => {
+      item.tasks || (item.tasks = []);
+
+      const finished = item.tasks.filter(task => task.status === 'TEST_PASSED').length;
+
+      xAxis.unshift(`${item.year}-${item.number}`);
+      finishedSeries.unshift(finished);
+      pendingSeries.unshift(item.tasks.length - finished);
+      total.unshift(item.tasks.length);
+    });
+
+    chart.setOption({
+      color: ['#4db47d', '#fbca35', '#ff8acc'],
+      tooltip: {},
+      xAxis: {
+        data: xAxis
+      },
+      yAxis: {},
+      legend: {
+        data: ['Finished', 'Pending', 'Total']
+      },
+      series: [
+        {
+          name: 'Finished',
+          type: 'bar',
+          label: {
+            normal: {
+              show: true,
+              textStyle: {
+                fontSize: 18
+              }
+            }
+          },
+          data: finishedSeries
+        },
+        {
+          name: 'Pending',
+          type: 'bar',
+          label: {
+            normal: {
+              show: true,
+              textStyle: {
+                fontSize: 18
+              }
+            }
+          },
+          data: pendingSeries
+        },
+        {
+          name: 'Total',
+          type: 'line',
+          data: total,
+          lineStyle: {
+            normal: {
+              width: 8
+            }
+          },
+          markPoint: {
+            data: [
+              { type: 'max', name: '最大值' },
+              { type: 'min', name: '最小值' }
+            ]
+          },
+          markLine: {
+            data: [
+              { type: 'average', name: '平均值' }
+            ]
+          }
+        }
+      ]
+    });
+  }
+
 	render() {
+    const {
+      iteration: { iterationList = [] }
+    } = this.props;
 
 		return (
 			<main>
 				<header className="page-header">
 					<h1>Dashboard</h1>
 				</header>
-			</main>
+
+        <div className="card">
+          <h2>Tasks</h2>
+
+          <section ref="taskChart" style={{height: 500}} />
+        </div>
+      </main>
 		);
 	}
 }
